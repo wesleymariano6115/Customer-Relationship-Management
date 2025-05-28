476f8836246ae89a312ccbf9b6ecc83b5a1e0fb8
Wiii-1 <161140066+Wiii-1@users.noreply.github.com>
2025-05-28 10:04:28 +0000

    deleting

--- Backend/src/main/java/com/example/demo/DTO/AuthResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.DTO;
-
-public class AuthResponse {
-    private String token;
-
-    public AuthResponse(String token) {
-            this.token = token;
-    }
-}
--- Backend/src/main/java/com/example/demo/Model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.Model;
-
-public enum Role {
-    ADMIN,
-    SALES_REP
-}
