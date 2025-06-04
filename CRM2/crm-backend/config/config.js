00644e36028ca9178abdcf857253f8ff6f232a11
wiii-1 <markejunemarino@gmail.com>
2025-06-04 18:55:58 +0800

    remove

--- CRM2/crm-backend/config/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  port: process.env.PORT || 5000,
-  databaseUrl: process.env.DATABASE_URL,
-  jwtSecret: process.env.JWT_SECRET,
-};
--- CRM2/crm-backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (id, role) => {
-  return jwt.sign({ id, role }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-  });
-};
-
-module.exports = generateToken;
